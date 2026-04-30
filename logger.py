351c8a11615f20f7d68b72c8440e9d3583fde9ca
Chris Davidson <149519762+ggd466@users.noreply.github.com>
2026-04-30 04:00:18 +0100

    Refactored implement rotating logger setup

--- logger.py
+++ logger.py
@@ -1,29 +1,22 @@
 import logging
+import os
 from logging.handlers import RotatingFileHandler
 
-class CustomLogger:
-    def __init__(self, name, log_file, max_bytes=5*1024*1024, backup_count=3):
-        self.logger = logging.getLogger(name)
-        self.logger.setLevel(logging.DEBUG)
+def setup_logger(log_file='app.log', max_bytes=10**6, backup_count=5):
+    logger = logging.getLogger('rotating_logger')
+    logger.setLevel(logging.DEBUG)
+    if not logger.handlers:
+        # Create a directory for logs if it doesn't exist
+        log_dir = os.path.dirname(log_file)
+        if log_dir and not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+        # Create a rotating file handler
         handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
         handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
+        logger.addHandler(handler)
+    return logger
 
-    def debug(self, msg):
-        self.logger.debug(msg)
-
-    def info(self, msg):
-        self.logger.info(msg)
-
-    def warning(self, msg):
-        self.logger.warning(msg)
-
-    def error(self, msg):
-        self.logger.error(msg)
-
-    def critical(self, msg):
-        self.logger.critical(msg)
-
-logger = CustomLogger('roblox_tools', 'roblox_tools.log')
-logger.info('Logger setup complete')
+if __name__ == '__main__':
+    logger = setup_logger()
+    logger.info('Logger is set up and ready to go!')
